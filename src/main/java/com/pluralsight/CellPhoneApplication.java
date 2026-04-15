3e46b0a7391a3ea2537bcc6583cb6839cf03d585
itkhari <mrkharij1@gmail.com>
2026-04-15 14:07:25 -0400

    actually added user inputs

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
-          /*- serialNumber (ex: 1000000 - 9999999)
-      - model (ex: iPhone X)
-      - carrier (ex: AT&T)
-      - phoneNumber (ex: 800-555-5555)
-      - owner (ex: Dana Wyatt)
-   */
+        Scanner sc = new Scanner(System.in);
 
-       /* What is the serial number? 2597153
+        /* What is the serial number? 2597153
         What model is the phone? iPhone 15 Pro Max
         Who is the carrier? Verizon
         What is the phone number? 888-555-1234
@@ -21,12 +16,23 @@ public class CellPhoneApplication {
 
         CellPhone phone1 = new CellPhone(2597153, "IPhone 15 Pro Max", "Verizon", "888-555-1234", "Sandra");
 
-       System.out.println("What is the serial number?: " + phone1.getSerialNumber());
-       System.out.println("What is the model?: " + phone1.getModel());
-       System.out.println("Who is the carrier?:  " + phone1.getCarrier());
-       System.out.println("What is the phone number?: " + phone1.getPhoneNumber());
-       System.out.println("Who is the owner of the phone?: " + phone1.getOwner());
+       System.out.print("What is the serial number?: ");
+       phone1.setSerialNumber(sc.nextInt());
+       sc.nextLine();
+       System.out.print("What is the model?: ");
+       phone1.setModel(sc.nextLine());
+       System.out.print("Who is the carrier?: ");
+       phone1.setCarrier(sc.nextLine());
+       System.out.print("What is the phone number?: ");
+       phone1.setPhoneNumber(sc.nextLine());
+       System.out.print("Who is the owner of the phone?: ");
+       phone1.setOwner(sc.nextLine());
 
+        System.out.println(phone1.getSerialNumber());
+        System.out.println(phone1.getModel());
+        System.out.println(phone1.getCarrier());
+        System.out.println(phone1.getPhoneNumber());
+        System.out.println(phone1.getOwner());
 
     }
 }
